01db9df60492aa7531ddaa8059cb102901c82652
yoosaemsol <yoosaemsol@gmail.com>
2021-11-12 17:44:41 +0900

    my first POST request

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+// to get body data from POST
+const bodyParser = require('body-parser');
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(8080, function () {
   console.log('listening on 8080');
 });
@@ -11,4 +15,13 @@ app.get('/abc', function (req, res) {
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/write', function (req, res) {
+  res.sendFile(__dirname + '/write.html');
+});
+
+app.post('/add', function (req, res) {
+  res.send('Success');
+  console.log(req.body.title);
 });
